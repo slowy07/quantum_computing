2029c295d646d985b97dd1ed76a831c9dc2d62bd
slowy07 <slowy.arfy@gmail.com>
2021-12-17 23:57:58 +0700

    feat: adding half adder
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ half_adder/half_adder.py
@@ -0,0 +1,48 @@
+# Build a half-adder quantum circuit that takes two bits as input,
+# encodes them into qubits, then runs the half-adder circuit calculating
+# the sum and carry qubits, observed over 1000 runs of the experiment
+
+# https://en.wikipedia.org/wiki/Adder_(electronics)
+
+import qiskit as q
+
+
+def half_adder(bit0: int, bit1: int) -> q.result.count.Counts:
+    """
+    >>> half_adder(0, 0)
+    {'00': 1000}
+    """
+    # user Aer's qasm_simulator
+    simulator = q.Aer.get_backend("qasm_simulator")
+
+    qc_ha = q.QuantumCircuit(4, 2)
+
+    # encode inputs in qubits 0 and 1
+    if bit0 == 1:
+        qc_ha.x(0)
+    if bit1 == 1:
+        qc_ha.x(1)
+    qc_ha.barrier()
+
+    # use cnots to write XOR of the inputs on qubit2
+    qc_ha.cx(0, 2)
+    qc_ha.cx(1, 2)
+
+    # user ccx / toffoli gate to write AND of the inputs on qubit3
+    qc_ha.ccx(0, 1, 3)
+    qc_ha.barrier()
+
+    # extract outputs
+    qc_ha.measure(2, 0)  # extract XOR value
+    qc_ha.measure(3, 1)  # extract AND value
+
+    # execute the circuit on the qasm simulator
+    job = q.execute(qc_ha, simulator, shots=1000)
+
+    # return the histogram data of the result of the exeperiment.
+    return job.result().get_counts(qc_ha)
+
+
+if __name__ == "__main__":
+    counts = half_adder(1, 1)
+    print(f"half adder output qubit count {counts}")
