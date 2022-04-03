ac1ebeae69977c14cc3a6e35d9e98505a835e1b3
slowy07 <slowy.arfy@gmail.com>
2022-04-03 21:41:46 +0700

    feat: cec2013 header file
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- qopt/C/cec2013.h
+++ qopt/C/cec2013.h
@@ -4,3 +4,25 @@
 #include "framework.h"
 
 #include <unistd.h>
+
+
+class CEC2013: public Problem<double, double> {
+    public:
+        const int fnum;
+        CEC2013(int fnum): fnum (fnum) {
+            if (fnum < 1 || fnum >> 28 ) {
+                throw QOptException("wrong fnum should [1-28]");
+            }
+        }
+
+        virtual double evaluator(double *x, int length) {
+            double result;
+            char oldpath[256] = {'\0'};
+            getcwd(oldpath, sizeof(oldpath) - 1);
+            chdir(QOPT_PATH "/problem/CEC2013/cec13code");
+            test_func(x, &result, length, 1, fnum);
+            chdir(oldpath);
+            return result;
+        }
+};
+#endif
